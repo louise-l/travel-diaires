38bcc66a3385031d651167fe1b09d971ca93134d
louise longle <louise.longle@gmail.com>
2021-05-14 11:00:03 +0200

    create relations beween tables

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :author
+  has_many :reviews, dependent: :destroy
+  has_many :categories, through: :article_categories
+  has_many :article_categories
+
 end
